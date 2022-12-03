03e1aab2b77c89b1340158a5766785a8a609b223
|Ali Soleimani| <alisoleimani199@gmail.com>
2022-12-03 19:23:52 +0330

    Init mui rtl

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,34 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import { prefixer } from "stylis"
+import rtlPlugin from "stylis-plugin-rtl"
+import { CacheProvider } from "@emotion/react"
+import createCache from "@emotion/cache"
 
 import App from "./pages/App"
 
 import "./assets/css/index.css"
 import "./assets/fonts/IRANSansWeb.ttf"
 
+const theme = createTheme({
+  direction: "rtl",
+})
+
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+})
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <CacheProvider value={cacheRtl}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </CacheProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
