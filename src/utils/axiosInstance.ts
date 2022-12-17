52b135f1aec754af25aefce9fd6683b8c00fcfb2
|Ali Soleimani| <alisoleimani199@gmail.com>
2022-12-17 09:26:36 +0330

    Change axios base url

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios"
 
 export default (token: string): AxiosInstance => {
   return axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3000/api",
     headers: { "x-access-token": `Bearer ${token}` },
   })
 }
