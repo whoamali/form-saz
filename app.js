59e72e3d8321fc2a2805e9ab97b1321b8cc4220e
|Ali Soleimani| <alisoleimani199@gmail.com>
2022-12-17 09:19:04 +0330

    Init app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const connnectDB = require("./configs/db");
+
+const app = express();
+
+//* env config
+dotenv.config({ path: "./configs/config.env" });
+
+//* DB connection
+connnectDB();
+
+//* Body parser
+app.use(bodyParser.json());
+
+//* CORS
+app.use(cors());
+
+//* App static folders
+app.use(express.static(path.join(__dirname, "public")));
+
+//* Routes
+app.use(require("./routes/AUTH").singup);
+app.use(require("./routes/AUTH").login);
+app.use(require("./routes/AUTH").auth);
+
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on posrt ${PORT}`)
+);
