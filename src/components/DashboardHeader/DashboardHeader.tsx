9cf9c5d854a6ac740734c89c58bbda756e34087b
|Ali Soleimani| <alisoleimani199@gmail.com>
2022-12-17 11:21:38 +0330

    Set loading

--- src/components/DashboardHeader/DashboardHeader.tsx
+++ src/components/DashboardHeader/DashboardHeader.tsx
@@ -18,17 +18,25 @@ import {
 import Logout from "@mui/icons-material/Logout"
 import EditIcon from "@mui/icons-material/Edit"
 
-import { Logo } from "./../" //! /root/src/components
+import { Logo, Loading } from "./../" //! /root/src/components
 import { useAuth } from "./../../hooks"
 
 function ResponsiveAppBar() {
   const [data] = useAuth()
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null)
   const navigate = useNavigate()
+  const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
-    if (data.expired) {
-      navigate("/login")
+    if (data !== undefined) {
+      if (data.expired) {
+        navigate("/login")
+        setLoading(false)
+      } else {
+        setLoading(false)
+      }
+    } else {
+      setLoading(true)
     }
   }, [data])
 
@@ -36,95 +44,106 @@ function ResponsiveAppBar() {
     setAnchorElUser(event.currentTarget)
   }
 
-  const handleCloseUserMenu = (path: string | null) => {
+  const handleCloseUserMenu = (path: "edit" | "logout" | null) => {
     setAnchorElUser(null)
+    if (path === "logout") {
+      navigate("/logout")
+    }
   }
 
   return (
-    <AppBar
-      position="static"
-      sx={{ backgroundColor: "white", boxShadow: "none" }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar
-          sx={{
-            display: "flex",
-            alignItem: "center",
-            justifyContent: "space-between",
-          }}
-          disableGutters
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <AppBar
+          position="static"
+          sx={{ backgroundColor: "white", boxShadow: "none" }}
         >
-          <Stack direction="row" alignItems="center">
-            <Logo size="26px" />
-            <Typography
-              color="black"
-              fontSize="16px"
-              sx={{ display: { xs: "none", sm: "block" }, ml: "25px" }}
+          <Container maxWidth="xl">
+            <Toolbar
+              sx={{
+                display: "flex",
+                alignItem: "center",
+                justifyContent: "space-between",
+              }}
+              disableGutters
             >
-              ساخت فرم رایگان با چند کلیک!
-            </Typography>
-          </Stack>
+              <Stack direction="row" alignItems="center">
+                <Logo size="26px" />
+                <Typography
+                  color="black"
+                  fontSize="16px"
+                  sx={{ display: { xs: "none", sm: "block" }, ml: "25px" }}
+                >
+                  ساخت فرم رایگان با چند کلیک!
+                </Typography>
+              </Stack>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="تنظیمات حساب">
-              <IconButton
-                onClick={handleOpenUserMenu}
-                sx={{ p: 0 }}
-                disableRipple
-              >
-                <Avatar alt="Avatar" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              anchorEl={anchorElUser}
-              sx={{ mt: "45px" }}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={() => handleCloseUserMenu(null)}
-            >
-              <MenuItem
-                sx={{
-                  ":hover": {
-                    backgroundColor: "transparent",
-                    cursor: "default",
-                  },
-                }}
-                disableRipple
-              >
-                <Typography textAlign="center">{data.username}</Typography>
-              </MenuItem>
-              <Divider />
-              <MenuItem
-                sx={{ width: "200px" }}
-                onClick={() => handleCloseUserMenu("edit")}
-              >
-                <ListItemIcon>
-                  <EditIcon fontSize="small" />
-                </ListItemIcon>
-                <Typography textAlign="center">ویرایش</Typography>
-              </MenuItem>
-              <MenuItem
-                sx={{ width: "200px" }}
-                onClick={() => handleCloseUserMenu("logout")}
-              >
-                <ListItemIcon>
-                  <Logout fontSize="small" />
-                </ListItemIcon>
-                <Typography textAlign="center">خروج</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="تنظیمات حساب">
+                  <IconButton
+                    onClick={handleOpenUserMenu}
+                    sx={{ p: 0 }}
+                    disableRipple
+                  >
+                    <Avatar alt="Avatar" />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  anchorEl={anchorElUser}
+                  sx={{ mt: "45px" }}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={() => handleCloseUserMenu(null)}
+                >
+                  <MenuItem
+                    sx={{
+                      ":hover": {
+                        backgroundColor: "transparent",
+                        cursor: "default",
+                      },
+                    }}
+                    disableRipple
+                  >
+                    <Typography textAlign="center">
+                      {data && data.username}
+                    </Typography>
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem
+                    sx={{ width: "200px" }}
+                    onClick={() => handleCloseUserMenu("edit")}
+                  >
+                    <ListItemIcon>
+                      <EditIcon fontSize="small" />
+                    </ListItemIcon>
+                    <Typography textAlign="center">ویرایش</Typography>
+                  </MenuItem>
+                  <MenuItem
+                    sx={{ width: "200px" }}
+                    onClick={() => handleCloseUserMenu("logout")}
+                  >
+                    <ListItemIcon>
+                      <Logout fontSize="small" />
+                    </ListItemIcon>
+                    <Typography textAlign="center">خروج</Typography>
+                  </MenuItem>
+                </Menu>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      )}
+    </>
   )
 }
 export default ResponsiveAppBar
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,24 +1,49 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import { Box } from "@mui/material"
 
-import { Header, Footer } from "./../../components"
+import { Header, Footer, Loading } from "./../../components"
 import { Form } from "./components"
+import { useAuth } from "./../../hooks"
 
 interface IProps {
   title: string
 }
 
 export default function Login({ title }: IProps) {
+  const navigate = useNavigate()
+  const [data] = useAuth()
+  const [loading, setLoading] = useState<boolean>(true)
+
   //? Eddit page title
   useEffect(() => {
     document.title = title
   }, [])
 
+  useEffect(() => {
+    if (data !== undefined) {
+      if (!data.expired) {
+        navigate("/dashboard")
+        setLoading(false)
+      } else {
+        setLoading(false)
+      }
+    } else {
+      setLoading(true)
+    }
+  }, [data])
+
   return (
-    <Box component="main" sx={{ display: "flex", flexDirection: "column" }}>
-      <Header />
-      <Form />
-      <Footer />
-    </Box>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Box component="main" sx={{ display: "flex", flexDirection: "column" }}>
+          <Header />
+          <Form />
+          <Footer />
+        </Box>
+      )}
+    </>
   )
 }
