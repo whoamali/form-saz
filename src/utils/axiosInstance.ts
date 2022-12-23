78429757a0c4c2762e370af4b26ec6e01891207d
|Ali Soleimani| <alisoleimani199@gmail.com>
2022-12-24 00:37:17 +0330

    Change axios request headers

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -3,6 +3,6 @@ import axios, { AxiosInstance } from "axios"
 export default (token: string): AxiosInstance => {
   return axios.create({
     baseURL: "http://localhost:3000/api",
-    headers: { "x-access-token": `Bearer ${token}` },
+    headers: { "x-access-token": token },
   })
 }
