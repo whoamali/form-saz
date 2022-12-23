c611e898ba078bd24b24788c3f37e1d0bb37e27b
|Ali Soleimani| <alisoleimani199@gmail.com>
2022-12-24 00:36:47 +0330

    Create dashboard layout

---
+++ src/components/DashboardLayout/DashboardLayout.tsx
@@ -0,0 +1,25 @@
+import { Stack, Box, Typography } from "@mui/material"
+
+interface IProps {
+  headerText: string
+  headerElement?: JSX.Element
+  children: JSX.Element | string
+}
+
+export default function DashboardLayout({
+  headerText,
+  headerElement,
+  children,
+}: IProps) {
+  return (
+    <Stack sx={{ width: "100%", height: "100%", px: "50px", py: "30px", overflowY: "auto" }}>
+      <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{mb: "30px"}}>
+        <Typography variant="h3" fontSize="28px">
+          {headerText}
+        </Typography>
+        {headerElement}
+      </Stack>
+      <Box>{children}</Box>
+    </Stack>
+  )
+}
